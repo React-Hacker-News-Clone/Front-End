21547798484f602642aadc8df1e4b2ecabd6e26f
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-09-21 12:02:10 -0400

    Setup initial file structure

---
+++ src/utils/Routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PrivateRoute from "./utils/PrivateRoute";
+import { Route } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <div>
+      <h1>Routes</h1>
+      {/* Create All Routes here */}
+    </div>
+  );
+};
+
+export default Routes;
