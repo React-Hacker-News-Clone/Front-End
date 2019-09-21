56711d49986fb00d50f2f35f5d83e82f20a62d12
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-09-21 12:11:53 -0400

    Added favicon and removed boilerplating from CRA

--- src/utils/Routes.js
+++ src/utils/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PrivateRoute from "./utils/PrivateRoute";
+import PrivateRoute from "./PrivateRoute";
 import { Route } from "react-router-dom";
 
 const Routes = () => {
