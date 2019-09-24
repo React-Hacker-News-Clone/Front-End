a4ff4f0dacd6725e7fbb9cab253578bc3c3d3b3d
teddyhn <tngo97@gmail.com>
2019-09-23 21:32:15 -0700

    Setup sign in using socials
    Co-authored-by: Isaiah <42652624+FrancoisCoding@users.noreply.github.com>

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
-import { FacebookLoginButton, GoogleLoginButton, TwitterLoginButton} from "react-social-login-buttons";
 
 function LoginForm() {
   return (
@@ -8,9 +7,6 @@ function LoginForm() {
       <Field type="text" name="username" placeholder="Username" />
       <Field type="password" name="password" placeholder="Password" />
       <button>Log In</button>
-      <FacebookLoginButton onClick={() => alert("You're logging!")} />
-      <GoogleLoginButton onClick={() => alert("You're logging!")} />
-      <TwitterLoginButton onClick={() => alert("You're logging!")} />
     </Form>
   );
 }
